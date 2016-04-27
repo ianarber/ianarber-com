41602190f9702ed5419abfffa6fd90adc6ce1159
Anthony Desmier <adesmier@gmail.com>
2016-04-27 17:27:49 +0100

    =responsive header changes

--- assets/script/collapseHeader.js
+++ assets/script/collapseHeader.js
@@ -10,15 +10,35 @@ $('#welcome-intro').waypoint(function (direction) {
 		$('.menu-items-2').css({'fontSize': '65%'});
 		$('#header').animate({height: '60px'}, 300);
 	} else {
-		$('.ianarber').css({'fontSize': '250%'});
-		$('.ianarber').css({'top': '0px'});
-		$('#logo p:eq(0)').removeClass('jscomposer').addClass('composer');
-		$('#logo p:eq(2)').removeClass('jsfilm').addClass('film');
-		$('.menu-list-1').css({'marginTop': '30px'});
-		$('.menu-list-2').css({'marginTop': '30px'});
-		$('.menu-items-1').css({'fontSize': '75%'});
-		$('.menu-items-2').css({'fontSize': '75%'});
-		$('#header').animate({height: '120px'}, 300);
+		if ($(window).width() > 1075) {
+			$('.ianarber').css({'fontSize': '250%'});
+			$('.ianarber').css({'top': '0px'});
+			$('#logo p:eq(0)').removeClass('jscomposer').addClass('composer');
+			$('#logo p:eq(2)').removeClass('jsfilm').addClass('film');
+			$('.menu-list-1').css({'marginTop': '30px'});
+			$('.menu-list-2').css({'marginTop': '30px'});
+			$('.menu-items-1').css({'fontSize': '75%'});
+			$('.menu-items-2').css({'fontSize': '75%'});
+			$('#header').animate({height: '120px'}, 300);
+		}
+		if ($(window).width() <= 1075) {
+			$('.ianarber').css({'fontSize': '195%'});
+			$('.ianarber').css({'top': '0px'});
+			$('.composer').css({'fontSize': '55%'});
+			$('.film').css({'fontSize': '55%'});
+			$('#logo p:eq(0)').removeClass('jscomposer').addClass('composer');
+			$('#logo p:eq(2)').removeClass('jsfilm').addClass('film');
+			$('.menu-list-1').css({'marginTop': '17px'});
+			$('.menu-list-2').css({'marginTop': '17px'});
+			$('.menu-items-1').css({'fontSize': '65%'});
+			$('.menu-items-2').css({'fontSize': '65%'});
+			$('#header').animate({height: '100px'}, 300);
+		}
+		
+		
+		
+		
+		
 	}
 },{
 	offset: '100px'
