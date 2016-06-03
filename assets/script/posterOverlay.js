84bfbe148e33c7ebbf1eb71c3ee247f67eb9033a
Anthony Desmier <adesmier@gmail.com>
2016-06-03 17:27:08 +0100

    =poster overlay editing

--- assets/script/posterOverlay.js
+++ assets/script/posterOverlay.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
-	// handle the mouseenter functionality
-	$(".poster-figure").mouseenter(function () {
-			$(this).addClass("hover");
-		})
-		// handle the mouseleave functionality
-		.mouseleave(function () {
-			$(this).removeClass("hover");
-		});
+    // handle the mouseenter functionality
+    $(".poster-figure").mouseenter(function () {
+            $(this).addClass("hover");
+        })
+        // handle the mouseleave functionality
+        .mouseleave(function () {
+            $(this).removeClass("hover");
+        });
+
+    $(".credit-brief").each(function (i) {
+        len = $(this).text().length;
+        if (len > 110) {
+            $(this).text($(this).text().substr(0, 110) + '...');
+        }
+    });
+
+
 });
