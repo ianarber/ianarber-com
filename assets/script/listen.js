7bb81406b597f20cc45591b670eda9a5f57e295b
Anthony Desmier <adesmier@gmail.com>
2016-06-16 17:31:31 +0100

    =listen jquery code

--- assets/script/listen.js
+++ assets/script/listen.js
@@ -1,53 +1,41 @@
 $(function () {
-//	$('#nav-toggle').click(function () {
-//		$('#container').toggleClass('active');
-//		$('.mobile-nav').toggleClass('active');
-//	});
-//    
-//    /* TODO: move into different JS file */
-//    $('.fa-envelope, .tooltip-form').click(function(event) {
-//        event.stopPropagation();
-//        $('.tooltip-form').addClass('active');    
+    
+    $('#genres li').on('click', function(){
+        $('#genres').find('.added').removeClass('added');
+        $(this).addClass('added');
+        
+        var genre = $(this).attr('rel');
+        
+        var $liToShow = $('#listen-gallery').find('.sc-audio').not('.' + genre);
+        
+        $('#listen-gallery').find('.disabled').removeClass('disabled');
+
+        $liToShow.find('iframe').addClass('disabled');
+        $liToShow.find('.bg-iframe-info').addClass('disabled');
+        $liToShow.find('.fg-cover').addClass('disabled');
+    });
+    
+//    var playlistPos = $('.sc-play').scrollTop.offset().top -70;
+//    var ffilmsPos = $('.sc-ffilms').scrollTop.offset().top -70;
+//    var sfilmsPos = $('.sc-sfilms').scrollTop.offset().top -70;
+//    var tvPos = $('.sc-tv').scrollTop.offset().top -70;
+    
+    $('#categories li').on('click', function(){
+        $('#categories').find('.added').removeClass('added');
+        $(this).addClass('added');
+        
+        var category = $(this).attr('rel');
+        
+        $('html, body').animate({
+            scrollTop: $('.' + category).offset().top -70
+        }, 2000);
+    })
+    
+//    $(window).scroll(function(){
+//        var scroll = $(window).scrollTop();
+//        
+//        if( scroll >= playlistPos && scroll)
 //    });
-//    
-//    $(document).click(function () {
-//        $('.tooltip-form').removeClass('active');
-//    })
-    
-    $('#genres-all').on('click', function(){
-        $('#genres li').removeClass('added');
-        $('#genres-all').addClass('added');
-        
-        $('.sc-audio iframe, .sc-audio .bg-iframe-info, .sc-audio .fg-cover').removeClass('disabled');     
-    })
-    
-//    $('#genres-drama').on('click', function(){
-//        $('#genres li').removeClass('added');
-//        $('#genres-drama').addClass('added');
-//        
-//        $('.sc-audio').css('opacity', '1');
-//        $('.sc-audio').not('.sc-drama').css('opacity', '0.1');
-//    })
-    
-    $('#genres-actadv').on('click', function(){
-        $('#genres li').removeClass('added');
-        $('#genres-actadv').addClass('added');
-        
-        $('.sc-audio iframe, .sc-audio .bg-iframe-info, .sc-audio .fg-cover').removeClass('disabled');
-        $('.sc-audio iframe, .sc-audio .bg-iframe-info, .sc-audio .fg-cover').not('.sc-actadv iframe, .sc-actadv .bg-iframe-info, .sc-actadv .fg-cover').addClass('disabled');
-    })
-    
-//    $('#genres-epic').on('click', function(){
-//        $('#genres li').removeClass('added');
-//        $('#genres-epic').addClass('added');
-//        
-//        $('.sc-audio').css('opacity', '1');
-//        $('.sc-audio').not('.sc-epic').css('opacity', '0.1');
-//    })
-    
-    
-    
-    
-    
+  
     
 });
