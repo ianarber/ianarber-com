4d37a77ae5c91a20d2dec8d893e7b8f8147a9a22
Anthony Desmier <adesmier@gmail.com>
2016-07-03 22:51:37 +0100

    =listen section update

--- assets/script/listen.js
+++ assets/script/listen.js
@@ -1,41 +1,85 @@
 $(function () {
     
+    //jquery variables
+    var $genres = $('#genres');
+    var $listenGallery = $('#listen-gallery');
+    
+    var $categories = $('#categories');
+    
+    var $genreMenu = $('.genre-menu');
+    
     $('#genres li').on('click', function(){
-        $('#genres').find('.added').removeClass('added');
+        //highlight genre button the user has clicked on
+        $genres.find('.added').removeClass('added');
         $(this).addClass('added');
         
         var genre = $(this).attr('rel');
         
+        //store all LI elements apart from the one user has seledted genre for
         var $liToShow = $('#listen-gallery').find('.sc-audio').not('.' + genre);
         
-        $('#listen-gallery').find('.disabled').removeClass('disabled');
+        //reset disabled classes
+        $listenGallery.find('.disabled').removeClass('disabled');
+        $listenGallery.find('.temp-disable-image').removeClass('temp-disable-image');
 
+        //add disabled classes to LI elements not of the genre user has clicked on
+        $liToShow.find('img').addClass('temp-disable-image');
         $liToShow.find('iframe').addClass('disabled');
         $liToShow.find('.bg-iframe-info').addClass('disabled');
         $liToShow.find('.fg-cover').addClass('disabled');
     });
     
-//    var playlistPos = $('.sc-play').scrollTop.offset().top -70;
-//    var ffilmsPos = $('.sc-ffilms').scrollTop.offset().top -70;
-//    var sfilmsPos = $('.sc-sfilms').scrollTop.offset().top -70;
-//    var tvPos = $('.sc-tv').scrollTop.offset().top -70;
     
     $('#categories li').on('click', function(){
-        $('#categories').find('.added').removeClass('added');
+        //highlight category button the user has clicked on
+        $categories.find('.added').removeClass('added');
         $(this).addClass('added');
         
+        //store rel att data
         var category = $(this).attr('rel');
         
+        //animate scroll to class with name of rel data
         $('html, body').animate({
             scrollTop: $('.' + category).offset().top -70
         }, 2000);
     })
     
-//    $(window).scroll(function(){
-//        var scroll = $(window).scrollTop();
-//        
-//        if( scroll >= playlistPos && scroll)
+    
+    $('img').on('click', function(){
+
+        //set full opacity to click to load image (via css)
+        $(this).addClass('disable-image');
+        //search for nearest iframe
+        var $iframe = $(this).siblings('iframe');
+        //populate src att with data stored in data-src
+        var src = $iframe.attr('data-src');
+        $iframe.attr('src', src); 
+        //sc widget iframe will now load
+        
+    });
+    
+    
+//    $('#arrow').toggle(function(){
+//         $genreMenu.css('transform', 'translateX(100px)');
+//    }, function(){
+//         $genreMenu.css('transform', 'translateX(-410px)');
 //    });
+    
+    
+    $('#arrow').on('click', function(){
+         $genreMenu.css('transform', 'translateX(100px)');
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   
     
 });
