f2c24dcd06ddf2eb6ed6464ef3f81a08e7cffa9f
Anthony Desmier <adesmier@gmail.com>
2016-07-07 17:26:10 +0100

    =big template changes and image slider updates

--- assets/script/introSlider.js
+++ assets/script/introSlider.js
@@ -1,51 +1,80 @@
 $(function () {
-    
-//     $('#bitch img:gt(0)').hide();
-//    
-//     setInterval(function () {
-//         $('#bitch :first-child').fadeOut('slow')
-//             .next('img').fadeIn('slow')
-//             .end().appendTo('#bitch');
-//     },
-//     6000); 
-    
-    
-    
-//    $('#nav-scroll-btns li').on('click', function(){
-//        
-//        var slideToShow = $(this).attr('rel');
-//        
-//        var $imageToFadeIn = $('#bitch').find('#' + slideToShow);
-//        var $imageToFadeOut = $('#bitch').not('#' + slideToShow);
-//        
-//        $imageToFadeIn.fadeIn('slow');
-//        $imageToFadeOut.fadeOut('slow');
-//    })
 
-    var count = $('.img-slide > img').length;
     var $navBtns = $('#nav-scroll-btns li');
     var current = 1;
     var sliderNextNum = 2;
-    var nextSlide;
-    var relToMatch;
 
+    //initally fade out all images
     $("img[id^='img-slide-']").fadeOut(0);
-    $('#img-slide-' + current).fadeIn(1000);
-    var loop = setInterval(function () {
-        $('#img-slide-' + current).fadeOut(1000);
+    //fade in first slide
+    $('#img-slide-' + current).fadeIn(1000);  
+    //start slider loop
+    var loop = setInterval(imageSlider, 7000);
+    
+    //interval function
+    function imageSlider(){
         
+        var count = $('.img-slide > img').length;
+        var relToMatch;
+        var nextSlide;
+        //fade out current slide
+        $('#img-slide-' + current).fadeOut(1000).removeClass('active');
+        //set next slide variable
         nextSlide = '#img-slide-' + sliderNextNum;
-        
-        $(nextSlide).fadeIn(1000);
-        
+        //fade the next slide in
+        $(nextSlide).fadeIn(1000).addClass('active');
+        //grad id value to match against nav button rel value
         relToMatch = $(nextSlide).attr('id');
-        
+        //call function to set nav buttons active
         highlightNavBtns(relToMatch);
-        
+        //increment or reset variables ready for next iteration
         (sliderNextNum >= count) ? sliderNextNum = 1: sliderNextNum++;
         (current >= count) ? current = 1: current++;
-    }, 7000)
+    }
     
+
+    $navBtns.on('click', function () {
+        //cancel the interval
+        stopInterval()
+        //increment index value
+        var index = $(this).index() + 1;
+        
+        //only execute if current slide is different from clicked nav button
+        if( current != index ){
+            //remove all active classes from img elements
+            $("img[id^='img-slide-']").fadeOut(1000).removeClass('active');
+            //fade in img based on index value
+            $('#img-slide-' + index).fadeIn(1000).addClass('active');
+            //add and remove class on nav buttons
+            $navBtns.removeClass('active');
+            $(this).addClass('active');
+        }
+        
+        //set variables based on index uder clicked
+        current = index;
+        sliderNextNum = ++index;
+        //check for end of element array
+        if( sliderNextNum > 4 ){
+            sliderNextNum = 1;
+        }
+        //restart interval
+        startInterval();
+    });
+
+    
+    function stopInterval(){
+        if( loop != undefined ){
+            clearInterval(loop);         
+        }
+    }
+
+    
+    function startInterval(){
+        if( loop != undefined ){
+            loop = setInterval(imageSlider, 7000);
+        }
+    }
+
     
     function highlightNavBtns(imageRel){
         $navBtns.each(function(){
@@ -58,23 +87,15 @@ $(function () {
     }
     
     
-    $('#nav-scroll-btns li').bind('click', function () {
-
-        var index = $(this).index() + 1;
-
-        $('.img-slider-active').fadeOut(1000);
-
-        $('#img-slide-' + index).fadeIn(1000);
-        $('.img-slide').removeClass('img-slider-active');
-        $('#img-slide-' + index).addClass('img-slider-active');
-        
-        current = index;
-        sliderNextNum = index++;
-        
-    });
-
-
 });
+
+
+
+
+
+
+
+
 
 
 
