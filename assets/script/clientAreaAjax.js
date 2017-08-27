1eb73eeaf28826a48332218603fea44daec60557
Anthony Desmier <adesmier@gmail.com>
2017-08-27 12:39:38 +0100

    update client area page

--- assets/script/clientAreaAjax.js
+++ assets/script/clientAreaAjax.js
@@ -8,14 +8,14 @@ function getWebtaskData(){
 
         $.ajax({
             type : 'GET',
-            url : 'https://webtask.it.auth0.com/api/run/wt-26212ff75758b7d16d19104dea3bca60-0/subscribers/subscribers',
+            url : 'https://webtask.it.auth0.com/api/run/wt-26212ff75758b7d16d19104dea3bca60-0/subscribers/clientarea',
             headers : {
                 Authorization : 'Bearer ' + localStorage.getItem('ianarber-auth0-access-token')
             }
         }).done(function(data) {
-            for(var i = 0; i < data.length; i++){
-                $('#subscribers').append('<h4>' + data[i] + '</h4>');
-            }
+            //for(var i = 0; i < data.length; i++){
+                $('#subscribers').append('<p>' + JSON.stringify(data) + '<p>');
+            //}
         }).fail(function(jqxhr, textStatus, error){
             var err = textStatus + ', ' + error;
             console.log('Request failed with error: ' + err);
