f23fbf7bd4d67e2a24ae5ac06a7eb0a9df95f974
Anthony Desmier <adesmier@gmail.com>
2018-01-05 15:51:31 +0000

    #4 Changed styling of lock widget

--- assets/script/modules/clientAreaAuth.js
+++ assets/script/modules/clientAreaAuth.js
@@ -10,17 +10,28 @@ $(function(){
         return json;
     }
 
+    /*
+      We first check if there is an item called 'ianarber-auth0-profile' in local storage.
+      If there then check if the expires key date is still within 24 hours.
+      If so the profile is still valid so the user does not have to log in again.
+      They are taken to the client area page.
+
+      NOTE: Only stage 1 of authentication and not really secure as you can just add an
+      item into local storage yourself. The real authentication comes from the ajax call
+      to the webtask to get the data to display on the client area page. If the ajax
+      authentication fails then the user will be asked to login
+    */
     function openAuth0Lock(event){
         event.preventDefault();
         var user = JSON.parse(localStorage.getItem('ianarber-auth0-profile'));
 
-        if(user && user.expires){ //first check if profile email stored is correct
+        if(user && user.expires){
             var expiresDate = Date.parse(user.expires.date);
             var date = new Date();
             date.setDate(date.getDate());
             var currentDate = Date.parse(date.toJSON());
             if(expiresDate >= currentDate){ //and then if it's still valid
-                window.location = '/clientarea';
+                window.location = '/reel';
             }  else {
                 auth0lock.show();
             }
@@ -29,7 +40,8 @@ $(function(){
         }
     }
     
-    // theme, style and auth options for auth0 lock widget https://auth0.com/docs/libraries/lock/v10/customization
+    // theme, style and auth options for auth0 lock widget
+    // https://auth0.com/docs/libraries/lock/v10/customization
     var auth0LockOptions = {
         rememberLastLogin: false,
         allowForgotPassword: false,
@@ -37,7 +49,6 @@ $(function(){
             email: 'guest@ianarber.com'
         },
         theme: {
-            logo: '/assets/img/ian_headshot_auth.jpg', //TODO: update
             primaryColor: '#0d2f52',
             labeledSubmitButton: false
         },
@@ -46,7 +57,7 @@ $(function(){
             title: "Client Area"
         },
         auth: {
-            redirectUrl: 'http://192.168.1.145:3000/clientarea', //TODO: update
+            redirectUrl: 'http://192.168.1.145:3002/reel', //TODO: update
             responseType: 'token id_token'
         }
     };
@@ -54,29 +65,22 @@ $(function(){
     //create new auth0 lock widget with options
     var auth0lock = new Auth0Lock('TdKEaCjzuCd5uiQfnq8CpJuJoZSoGSg0', 'ianarber.auth0.com', auth0LockOptions);
     auth0lock.on('authenticated', function(authResult) {
+        //once authenticated...
         auth0lock.getUserInfo(authResult.accessToken, function(error, profile) {
           if (error) {
-            return alert(error.message); //TODO: don't use alert here
+            return alert(error.message); //TODO: handle error better
           }
 
           profile = insertExpiresDate(profile); //valid for 24 hours
-          localStorage.setItem('ianarber-auth0-access-token', authResult.idToken); //can be used to call secure webtask function
+          localStorage.setItem('ianarber-auth0-access-token', authResult.idToken);
           localStorage.setItem('ianarber-auth0-profile', JSON.stringify(profile));
+          //token is now stored to send to webtask
         });
     });
 
     var $clientAreaBtns = $('.client-area-btn'); //run for all client area links
-    var $clearAuthItems = $('#clear-auth-items'); //TODO: Needs to be removed
+    //var $clearAuthItems = $('#clear-auth-items'); //TODO: Needs to be removed
 
-    /*
-      We first check if there is an item called 'ianarber-auth0-profile' in local storage. If there is it's email key needs to equal the value
-      in allowedEmail. If it does then check if the expires key date is still within 24 hours. If so the profile is still valid so the user
-      does not have to log in again. They are taken to the client area page.
-
-      NOTE: Only stage 1 of authentication and not really secure as you can just add an item into local storage yourself. The real authentication
-      comes from the ajax call to the webtask to get the data to display on the client area page. If the ajax authentication fails then the user
-      will be asked to login
-    */
     $clientAreaBtns.on('click', function(e){
         openAuth0Lock(e);
     });
@@ -90,13 +94,10 @@ $(function(){
     });
 
     //TODO: Needs to be removed
-    $clearAuthItems.on('click', function(e){
-        localStorage.removeItem('ianarber-auth0-profile');
-        localStorage.removeItem('ianarber-auth0-access-token');
-        console.log('Auth items cleared');
-    });
-
-
-
+    // $clearAuthItems.on('click', function(e){
+    //     localStorage.removeItem('ianarber-auth0-profile');
+    //     localStorage.removeItem('ianarber-auth0-access-token');
+    //     console.log('Auth items cleared');
+    // });
 
 });
